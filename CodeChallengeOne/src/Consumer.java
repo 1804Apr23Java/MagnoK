96d5f5200f1dfae1b0ba6be6a99f4964211e26a3
Kevin Magno <kmagno94@gmail.com>
2018-04-30 11:05:03 -0400

    Code Challenge #1 modified

--- CodeChallengeOne/src/Consumer.java
+++ CodeChallengeOne/src/Consumer.java
@@ -4,32 +4,32 @@ public class Consumer extends Thread {
 
 	public void run() {
 		// Continuous loop
-		while (true) {
-			try {
-				takeFromBasket();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+		try {
+			takeFromBasket();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 	}
 
 	public synchronized void takeFromBasket() throws InterruptedException {
-		Random r = new Random();
-		int howMuchToTake;
-		howMuchToTake = r.nextInt(33) + 1;
-
-		// Consumer waits for the basket to not be empty and for there to be enough to take
-		while (Basket.basket.size() == 0 || howMuchToTake > Basket.basket.size())
-			wait();
-
-		// Consumer takes amount from basket
-		for (int i = 0; i < howMuchToTake; i++) {
-			// Removes from head
-			System.out.println("Consumer consumed! : " + Basket.basket.get(i));
-			Basket.basket.removeFirst();
+		while(true) {
+			Random r = new Random();
+			int howMuchToTake;
+			howMuchToTake = r.nextInt(33) + 1;
+	
+			// Consumer waits for the basket to not be empty and for there to be enough to take
+			while (Basket.basket.size() == 0 || howMuchToTake > Basket.basket.size())
+				wait();
+	
+			// Consumer takes amount from basket
+			for (int i = 0; i < howMuchToTake; i++) {
+				// Removes from head
+				System.out.println("Consumer consumed! : " + Basket.basket.get(i));
+				Basket.basket.removeFirst();
+			}
+	
+			// Will alert the Producer to produce
+			notify();
 		}
-
-		// Will alert the Producer to produce
-		notify();
 	}
 }
