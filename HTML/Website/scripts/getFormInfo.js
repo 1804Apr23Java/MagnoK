55211a2e3c9a23228f3717b69da1bf8db1408ae9
Kevin Magno <kmagno94@gmail.com>
2018-05-11 18:11:20 -0400

    Project 1

--- HTML/Website/scripts/getFormInfo.js
+++ HTML/Website/scripts/getFormInfo.js
@@ -1,5 +1,8 @@
 
 function displayFormInfo() {
+    document.getElementById("exampleFormControlTextarea1");
+    text = exampleFormControlTextarea1.value;
+    console.log(text.value);
     document.getElementById("suggestedCities").innerHTML = "Comment submitted";
 };
 
@@ -7,7 +10,11 @@ function getComment() {
     document.getElementById("exampleFormControlTextarea1").value;
 };
 
-console.log(document.getElementById("exampleFormControlTextarea1").value);
+function getPokemon(xhr) {
+	var res = JSON.parse(xhr.responseText);
+	var html = "<h4>chosen pokemon has name: " + res.name + "</h4>";
+	document.getElementById("pokeResult").innerHTML = html;
+};
 
 window.onload = function () {
     displayFormInfo();
