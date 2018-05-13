878578ab2a2ae2b33e7dcdbdebb3c45c92f004dd
Kevin Magno <kmagno94@gmail.com>
2018-05-13 17:53:29 -0400

    Added Form functionality

--- HTML/Website/scripts/getFormInfo.js
+++ HTML/Website/scripts/getFormInfo.js
@@ -3,24 +3,45 @@ function displayFormInfo() {
     document.getElementById("exampleFormControlTextarea1");
     text = exampleFormControlTextarea1.value;
     console.log(text.value);
-    document.getElementById("suggestedCities").innerHTML = "Comment submitted";
-};
-
-function getComment() {
-    document.getElementById("exampleFormControlTextarea1").value;
-};
-
-function getPokemon(xhr) {
-	var res = JSON.parse(xhr.responseText);
-	var html = "<h4>chosen pokemon has name: " + res.name + "</h4>";
-	document.getElementById("pokeResult").innerHTML = html;
+    
 };
 
 window.onload = function () {
-    displayFormInfo();
-    document.getElementById("submit").addEventListener(
-        "click",
-        function () {
-            document.getElementById("exampleFormControlTextarea1").value;
-        });    
-} 
+    //displayFormInfo();
+    var commentForm = document.getElementById("submitButton");
+    commentForm.addEventListener("click",addNewComment);
+    // document.getElementById("submit").addEventListener(
+    //     "click",
+    //     function () {
+    //         document.getElementById("exampleFormControlTextarea1").value;
+    //     });    
+};
+
+function addNewComment(event) {
+    alert ("Hello!");
+    event.preventDefault();
+    var newComment = {
+        "email": document.getElementById('emailInput').value,
+        "comment": document.getElementById('commentInput').value
+    }
+    document.getElementById("suggestedCities").innerHTML = newComment.email + ":     " + newComment.comment;
+
+    console.log(newComment);
+    // var xhr = new XMLHttpRequest();
+    // xhr.open("POST",serverURL+"comment", true);
+    // xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState != 4 || xhr.status != 200) return;
+
+    //     console.log("Success: " + xhr.responseText);
+    //     commentForm.reset();
+    // };
+    // document.getElementById("suggestedCities").innerHTML = "Comment submitted";
+    // xhr.send(JSON.parse(newComment).innerHTML);
+}
+
+// function getPokemon(xhr) {
+// 	var res = JSON.parse(xhr.responseText);
+// 	var html = "<h4>chosen pokemon has name: " + res.name + "</h4>";
+// 	document.getElementById("pokeResult").innerHTML = html;
+// };
