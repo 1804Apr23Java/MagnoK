04d8dfd76e4041f365131b557d10509def958863
Kevin Magno <kmagno94@gmail.com>
2018-05-14 15:39:57 -0400

    HW

--- Week3CodeChallenge/scripts/bookshelf.js
+++ Week3CodeChallenge/scripts/bookshelf.js
@@ -1,7 +1,21 @@
 window.onload = function () {
     var commentForm = document.getElementById("submitButton");
     commentForm.addEventListener("click", addNewBook);
+    commentForm.addEventListener("click", addNewBookList);
 };
+
+function addNewBookList(event) {
+    // Create <li> node
+    var node = document.createElement("LI");
+    var newBook = {
+        "email": document.getElementById('emailInput').value,
+        "book": document.getElementById('bookInput').value
+    }
+    var info = document.createTextNode(newBook.book);
+    // Add book to 
+    node.appendChild(info);
+    document.getElementById("newBooks").appendChild(node);
+}
 
 function addNewBook(event) {
     event.preventDefault();
